5aec35283d0fa3a838c34b87889a4ab4af313609
Dasha <dariia.leshchenko@gmail.com>
2025-06-24 21:03:41 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     person_list = []
     for human in people:
         person_list.append(Person(human["name"], human["age"]))
